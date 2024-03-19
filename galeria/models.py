a5de27b5f776a6f0c1055f73317701311125ce82
Kaue Santana <kauesantana_13@hotmail.com>
2024-03-19 12:36:19 -0300

    Adicionando informações no banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -7,5 +7,6 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False) 
     foto = models.CharField(max_length=100, null=False, blank=False)
     
+    
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"

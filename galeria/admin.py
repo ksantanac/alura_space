6e2ced8ad7b03d91bd9d87db2b378d0aa74ad00c
Kaue Santana <kauesantana_13@hotmail.com>
2024-03-22 06:48:35 -0300

    .

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    # Forma de mostrar os campos do django admin
+    list_display = ("id", "nome", "legenda", "publicada")
+    
+    # Campos que possa clicar para editar
+    list_display_links = ( "id", "nome")
+    
+    # Adiciona um campo de buscar que procurará por nome
+    search_fields = ("nome",)
+    
+    # Criar um filtro por categoria
+    list_filter = ('categoria',)
+    
+    # Campo editável na lista
+    list_editable = ("publicada", )
+    
+    # Quantidade de itens para quebrar página
+    list_per_page = 10
+
+admin.site.register(Fotografia, ListandoFotografias)
